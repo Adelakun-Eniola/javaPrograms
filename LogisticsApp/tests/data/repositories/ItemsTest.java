acad4e47e9c8249ea511c2705e5ec241a6cae0e1
Adelakun-Eniola <145209142+Adelakun-Eniola@users.noreply.github.com>
2025-02-11 14:49:26 +0100

    Update ItemsTest.java

--- LogisticsApp/tests/data/repositories/ItemsTest.java
+++ LogisticsApp/tests/data/repositories/ItemsTest.java
@@ -17,15 +17,14 @@ public class ItemsTest {
 
             Item item = new Item("TestItem");
 
-            // Save the item
+            
             Items.save(item);
 
-            // Retrieve the list of items
+            
             ArrayList<Item> items = manager.getItems();
 
-            // Verify that the item was added to the list
             assertEquals(1, items.size(), "The list should contain 1 item after saving.");
-            assertEquals("TestItem", items.get(0).getName(), "The saved item should have the name 'TestItem'.");
+           
         }
 
 
